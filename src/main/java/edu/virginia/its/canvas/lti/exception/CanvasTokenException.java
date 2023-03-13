bc109c4c88cae2faa7270b9eb214067ca7a2e37f
Mitch Dunn <bmu7wk@virginia.edu>
2023-03-13 11:42:44 -0400

    LTI-41 LTI-56 Added static method to get the auth token

---
+++ src/main/java/edu/virginia/its/canvas/lti/exception/CanvasTokenException.java
@@ -0,0 +1,7 @@
+package edu.virginia.its.canvas.lti.exception;
+
+public class CanvasTokenException extends RuntimeException {
+  public CanvasTokenException(String message) {
+    super(message);
+  }
+}
