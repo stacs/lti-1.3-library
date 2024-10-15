59c1db92a84eec09e3bcbcfd020af643439203c3
Mitch Dunn <bmu7wk@virginia.edu>
2024-10-15 17:46:49 -0400

    LTI-233 Autoconfig changes from moving from Spring Boot 2 to 3

--- src/main/java/edu/virginia/its/canvas/lti/LtiConfig.java
+++ src/main/java/edu/virginia/its/canvas/lti/LtiConfig.java
@@ -1,11 +1,11 @@
 package edu.virginia.its.canvas.lti;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Configuration
+@AutoConfiguration
 @ComponentScan("edu.virginia.its.canvas")
 @EnableJpaRepositories("edu.virginia.its.canvas.lti.repos")
 @EntityScan("edu.virginia.its.canvas.lti.model")
