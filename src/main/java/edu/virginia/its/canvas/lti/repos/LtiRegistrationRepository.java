64c3b3105f5855a4a87a0975fa444b9cd6659796
Mitch Dunn <bmu7wk@virginia.edu>
2026-06-10 13:36:54 -0400

    LTI-68 Removed the DB way of establishing the LTI handshake in favor of the built-in default spring security way (via application.yaml)

--- src/main/java/edu/virginia/its/canvas/lti/repos/LtiRegistrationRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.virginia.its.canvas.lti.repos;
-
-import edu.virginia.its.canvas.lti.model.LtiRegistration;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LtiRegistrationRepository extends JpaRepository<LtiRegistration, Long> {
-  public LtiRegistration findByName(String name);
-}
