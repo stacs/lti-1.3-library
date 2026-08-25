36958d3147899fd91edee3ab3b12104e8f1f30ad
Mitch Dunn <bmu7wk@virginia.edu>
2026-08-25 15:14:44 -0400

    LTI-527 Updates for spring 7

--- src/main/java/edu/virginia/its/canvas/lti/LtiConfig.java
+++ src/main/java/edu/virginia/its/canvas/lti/LtiConfig.java
@@ -1,7 +1,7 @@
 package edu.virginia.its.canvas.lti;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
